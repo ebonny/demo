61e4b1b1b24a50fd70bb5214d14254e6ef64dd0e
이재철 <jclee@griffins.co.kr>
2019-05-18 18:31:01 +0900

    Update DemoApplication.java
    주석추라

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,8 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/* 수정함 */
+
 @SpringBootApplication
 public class DemoApplication {
 
